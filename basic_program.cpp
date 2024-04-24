bd163e9438f736cbb69b66e7620aafb7cabb06e6
Muktar Hosen <muktar.hosen@brainstation-23.com>
2024-04-24 19:20:32 +0600

    fibonacci series calculator function added

--- basic_program.cpp
+++ basic_program.cpp
@@ -12,3 +12,12 @@ unsigned long long factorial(int n) {
     else
         return n * factorial(n - 1);
 }
+
+unsigned long long fibonacci(int n) {
+    if (n == 0)
+        return 0;
+    else if (n == 1)
+        return 1;
+    else
+        return fibonacci(n - 1) + fibonacci(n - 2);
+}
