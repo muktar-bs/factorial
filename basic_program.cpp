2e37d68ca8cfdbcf5370d0b6f9889ee2060c5a20
Muktar Hosen <muktar.hosen@brainstation-23.com>
2024-04-24 19:36:01 +0600

    this commit will be cherry picked from main

--- basic_program.cpp
+++ basic_program.cpp
@@ -15,3 +15,11 @@ unsigned long long factorial(int n) {
     // this line is added in feature-branch to commit and then cherry-picking from main branch will be applied
     // this line is added after committing the changes by adding the above line of comment
 }
+
+
+unsigned long long sumUpTo(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sumUpTo(n - 1);
+}
