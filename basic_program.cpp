124297181a5f6854f257e4d588b79a2392db150a
Muktar Hosen <muktar.hosen@brainstation-23.com>
2024-04-24 19:36:01 +0600

    this commit will be cherry picked from main

--- basic_program.cpp
+++ basic_program.cpp
@@ -21,3 +21,19 @@ unsigned long long fibonacci(int n) {
     else
         return fibonacci(n - 1) + fibonacci(n - 2);
 }
+
+
+unsigned long long sumUpTo(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sumUpTo(n - 1);
+}
+
+
+unsigned long long sumUpTo(int n) {
+    if (n == 0)
+        return 0;
+    else
+        return n + sumUpTo(n - 1);
+}
