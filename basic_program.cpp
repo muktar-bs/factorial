e6b53e5be8788b4afa1ccbdd44938ddd130a290b
Muktar Hosen <muktar.hosen@brainstation-23.com>
2024-04-25 12:14:56 +0600

    This is a commit after applying stash from main branch and reslving the arised conflict

--- basic_program.cpp
+++ basic_program.cpp
@@ -1,25 +1,7 @@
-#include <iostream>
+#include<iostream>
 
 using namespace std;
 
 int main(){
     cout<<"Hello World";
-}
-
-unsigned long long factorial(int n) {
-    if (n == 0)
-        return 1;
-    else
-        return n * factorial(n - 1);
-
-    // this line is added in feature-branch to commit and then cherry-picking from main branch will be applied
-    // this line is added after committing the changes by adding the above line of comment
-}
-
-
-unsigned long long sumUpTo(int n) {
-    if (n == 0)
-        return 0;
-    else
-        return n + sumUpTo(n - 1);
 }
