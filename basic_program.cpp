8ba553a194ee284481606cfefe323a138efe57b5
Muktar Hosen <muktar.hosen@brainstation-23.com>
2024-04-25 11:47:40 +0600

    this is a faulty commit in factorial function

--- basic_program.cpp
+++ basic_program.cpp
@@ -5,3 +5,10 @@ using namespace std;
 int main(){
     cout<<"Hello World";
 }
+
+unsigned long long factorial(int n) {
+    if (n == 0)
+        return 1;
+    else
+        return n * factorial(n + 1);
+}
